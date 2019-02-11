0b453d20e76e6054af20023dcb731713fb50c774
Liza <shestakova.e.y@gmail.com>
2019-02-11 17:26:37 +0300

    Вернула скрипт в предыдущее состояние

--- js/script.js
+++ js/script.js
@@ -1,58 +1,71 @@
-"use strict";
-
-require("core-js/modules/web.dom.iterable");
-
 window.addEventListener('DOMContentLoaded', function () {
+
   'use strict';
 
-  var writeMe = document.querySelectorAll('.write-me'),
+  let writeMe = document.querySelectorAll('.write-me'),
       popup = document.querySelector('.popup'),
       close = document.querySelectorAll('.form-close');
+
   writeMe.forEach(function (item) {
-    item.addEventListener('click', function (event) {
-      event.preventDefault();
-      popup.classList.add('overlay');
-      document.body.style.overflow = 'hidden';
-    });
+      item.addEventListener('click', function (event) {
+          event.preventDefault();
+          popup.classList.add('overlay');
+          document.body.style.overflow = 'hidden';
+      });
   });
+
   close.forEach(function (item) {
-    item.addEventListener('click', function () {
-      this.parentNode.parentNode.classList.remove('overlay');
-      document.body.style.overflow = '';
-    });
+      item.addEventListener('click', function () {
+          this.parentNode.parentNode.classList.remove('overlay');
+          document.body.style.overflow = '';
+      });
   });
+
   var $page = $('html, body');
   $('a[href*="#"]').click(function () {
-    if (this.hash == '#top') {
-      $page.animate({
-        scrollTop: 0
-      }, 400);
-    } else {
-      $page.animate({
-        scrollTop: $($.attr(this, 'href')).offset().top - 70
-      }, 400);
-    }
-
-    return false;
-  });
-  $(function () {
-    $(window).scroll(function () {
-      if ($(this).scrollTop() != 0) {
-        $('#toTop').fadeIn();
-      } else {
-        $('#toTop').fadeOut();
+          if (this.hash == '#top') {
+              $page.animate({scrollTop: 0}, 400);
+          }
+          else {
+          $page.animate({
+              scrollTop: $($.attr(this, 'href')).offset().top - 70
+          }, 400);
       }
-    });
+      return false;
   });
-}); // // Код учителя:
+
+$(function () {
+
+  $(window).scroll(function () {
+
+      if ($(this).scrollTop() != 0) {
+
+          $('#toTop').fadeIn();
+
+      } else {
+
+          $('#toTop').fadeOut();
+
+      }
+
+  });
+
+
+});
+
+});
+// // Код учителя:
 // $(document).ready(function() {
+
 //     $('.write-me').click(function(event) {
 //         event.preventDefault();
 //         $('.popup').fadeIn(1000);
 //         // $('.modal').slideDown(1000);
 //     });
+
 //     $('.form-close').click(function() {
 //         // $('.modal').slideUp(1000);
 //         $('.popup').fadeOut(1000);
 //     });
+
 // });

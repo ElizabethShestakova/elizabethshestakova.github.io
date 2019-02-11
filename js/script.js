90505b4ff2a56fc80e7d33b99917aec29327bc0a
Liza <shestakova.e.y@gmail.com>
2019-02-11 12:36:10 +0300

    Установили Babel

--- js/script.js
+++ js/script.js
@@ -1,71 +1,58 @@
+"use strict";
+
+require("core-js/modules/web.dom.iterable");
+
 window.addEventListener('DOMContentLoaded', function () {
+  'use strict';
 
-    'use strict';
-
-    let writeMe = document.querySelectorAll('.write-me'),
-        popup = document.querySelector('.popup'),
-        close = document.querySelectorAll('.form-close');
-
-    writeMe.forEach(function (item) {
-        item.addEventListener('click', function (event) {
-            event.preventDefault();
-            popup.classList.add('overlay');
-            document.body.style.overflow = 'hidden';
-        });
+  var writeMe = document.querySelectorAll('.write-me'),
+      popup = document.querySelector('.popup'),
+      close = document.querySelectorAll('.form-close');
+  writeMe.forEach(function (item) {
+    item.addEventListener('click', function (event) {
+      event.preventDefault();
+      popup.classList.add('overlay');
+      document.body.style.overflow = 'hidden';
     });
-
-    close.forEach(function (item) {
-        item.addEventListener('click', function () {
-            this.parentNode.parentNode.classList.remove('overlay');
-            document.body.style.overflow = '';
-        });
+  });
+  close.forEach(function (item) {
+    item.addEventListener('click', function () {
+      this.parentNode.parentNode.classList.remove('overlay');
+      document.body.style.overflow = '';
     });
+  });
+  var $page = $('html, body');
+  $('a[href*="#"]').click(function () {
+    if (this.hash == '#top') {
+      $page.animate({
+        scrollTop: 0
+      }, 400);
+    } else {
+      $page.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top - 70
+      }, 400);
+    }
 
-    var $page = $('html, body');
-    $('a[href*="#"]').click(function () {
-            if (this.hash == '#top') {
-                $page.animate({scrollTop: 0}, 400);
-            }
-            else {
-            $page.animate({
-                scrollTop: $($.attr(this, 'href')).offset().top - 70
-            }, 400);
-        }
-        return false;
-    });
-
-$(function () {
-
+    return false;
+  });
+  $(function () {
     $(window).scroll(function () {
-
-        if ($(this).scrollTop() != 0) {
-
-            $('#toTop').fadeIn();
-
-        } else {
-
-            $('#toTop').fadeOut();
-
-        }
-
+      if ($(this).scrollTop() != 0) {
+        $('#toTop').fadeIn();
+      } else {
+        $('#toTop').fadeOut();
+      }
     });
-
-
-});
-
-});
-// // Код учителя:
+  });
+}); // // Код учителя:
 // $(document).ready(function() {
-
 //     $('.write-me').click(function(event) {
 //         event.preventDefault();
 //         $('.popup').fadeIn(1000);
 //         // $('.modal').slideDown(1000);
 //     });
-
 //     $('.form-close').click(function() {
 //         // $('.modal').slideUp(1000);
 //         $('.popup').fadeOut(1000);
 //     });
-
 // });

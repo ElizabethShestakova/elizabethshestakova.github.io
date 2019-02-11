ed3ee3eef997cda1fe9e587467c83eef437c516f
Liza <shestakova.e.y@gmail.com>
2019-02-11 18:01:07 +0300

    Добавлена строчка в опыт работы

--- js/script.js
+++ js/script.js
@@ -1,54 +1,54 @@
 window.addEventListener('DOMContentLoaded', function () {
 
-  'use strict';
+    'use strict';
 
-  let writeMe = document.querySelectorAll('.write-me'),
-      popup = document.querySelector('.popup'),
-      close = document.querySelectorAll('.form-close');
+    let writeMe = document.querySelectorAll('.write-me'),
+        popup = document.querySelector('.popup'),
+        close = document.querySelectorAll('.form-close');
 
-  writeMe.forEach(function (item) {
-      item.addEventListener('click', function (event) {
-          event.preventDefault();
-          popup.classList.add('overlay');
-          document.body.style.overflow = 'hidden';
-      });
-  });
+    writeMe.forEach(function (item) {
+        item.addEventListener('click', function (event) {
+            event.preventDefault();
+            popup.classList.add('overlay');
+            document.body.style.overflow = 'hidden';
+        });
+    });
 
-  close.forEach(function (item) {
-      item.addEventListener('click', function () {
-          this.parentNode.parentNode.classList.remove('overlay');
-          document.body.style.overflow = '';
-      });
-  });
+    close.forEach(function (item) {
+        item.addEventListener('click', function () {
+            this.parentNode.parentNode.classList.remove('overlay');
+            document.body.style.overflow = '';
+        });
+    });
 
-  var $page = $('html, body');
-  $('a[href*="#"]').click(function () {
-          if (this.hash == '#top') {
-              $page.animate({scrollTop: 0}, 400);
-          }
-          else {
-          $page.animate({
-              scrollTop: $($.attr(this, 'href')).offset().top - 70
-          }, 400);
-      }
-      return false;
-  });
+    var $page = $('html, body');
+    $('a[href*="#"]').click(function () {
+            if (this.hash == '#top') {
+                $page.animate({scrollTop: 0}, 400);
+            }
+            else {
+            $page.animate({
+                scrollTop: $($.attr(this, 'href')).offset().top - 70
+            }, 400);
+        }
+        return false;
+    });
 
 $(function () {
 
-  $(window).scroll(function () {
+    $(window).scroll(function () {
 
-      if ($(this).scrollTop() != 0) {
+        if ($(this).scrollTop() != 0) {
 
-          $('#toTop').fadeIn();
+            $('#toTop').fadeIn();
 
-      } else {
+        } else {
 
-          $('#toTop').fadeOut();
+            $('#toTop').fadeOut();
 
-      }
+        }
 
-  });
+    });
 
 
 });
